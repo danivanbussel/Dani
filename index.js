7816b47ce6ee82881b2991abdb798d62fe3c1488
danivanbussel <64401680+danivanbussel@users.noreply.github.com>
2020-04-28 10:25:59 +0200

    f
    c

--- index.js
+++ index.js
@@ -27,15 +27,6 @@ fs.readdir("./commands/", (err, files) => {
 
 
 
-
-
-
-
-
-
-
-
-
 // bot status (NL : Community is online!)
 bot.on("ready", async () => {
         console.log(`RaceTopia bot is online!`);
@@ -73,59 +64,7 @@ bot.on("ready", async () => {
 
 
 
-                const { Client, Collection } = require("discord.js");
-
-                
-const fs = require("fs");
-
-const client = new Client({
-    disableEveryone: true
-});
-
-client.commands = new Collection();
-client.aliases = new Collection();
-
-client.categories = fs.readdirSync("./commands/");
-
-config({
-    path: __dirname + "/.env"
-});
-
-["command"].forEach(handler => {
-    require(`./handlers/${handler}`)(client);
-});
-
-client.on("ready", () => {
-    console.log(`Hi, ${client.user.username} is now online!`);
-
-    client.user.setPresence({
-        status: "online",
-        game: {
-            name: "me getting developed",
-            type: "STREAMING"
-        }
-    }); 
-});
-
-client.on("message", async message => {
-    const prefix = "_";
-
-    if (message.author.bot) return;
-    if (!message.guild) return;
-    if (!message.content.startsWith(prefix)) return;
-    if (!message.member) message.member = await message.guild.fetchMember(message);
-
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
-    const cmd = args.shift().toLowerCase();
-    
-    if (cmd.length === 0) return;
-    
-    let command = client.commands.get(cmd);
-    if (!command) command = client.commands.get(client.aliases.get(cmd));
-
-    if (command) 
-        command.run(client, message, args);
-});
+ 
 
 
 
