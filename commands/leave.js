ef38b9af4ab4e28982ccdbdf7a4c5f0addb551cd
danivanbussel <64401680+danivanbussel@users.noreply.github.com>
2020-04-29 10:01:54 +0200

    s
    s

---
+++ commands/leave.js
@@ -0,0 +1,24 @@
+const discord = require("discord.js");
+
+module.exports.run = async(bot, message, args) => {
+
+    bot.on("guildMemberRemove", member => {
+ 
+        const channel = member.guild.channels.find("name", "report");
+        if (!channel) console.log("Kan het kanaal niet vinden.");
+     
+        var joinEmbed = new discord.RichEmbed()
+            .setAuthor(`${member.user.tag}`, member.user.displayAvatarURL)
+            .setColor("#FF0000")
+            .setTimestamp()
+            .setFooter("Gebruiker Geleaved.");
+     
+        channel.send(joinEmbed);
+     
+    });
+
+}
+
+module.exports.help = {
+    name: "leave"
+}
